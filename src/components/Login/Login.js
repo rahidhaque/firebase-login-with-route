fe9d9ce6683cdeba7c9f9b867fd1cd7680d957bc
rahidhaque <ontirahid@yahoo.com>
2022-04-08 23:50:12 +0600

    sign out implemented with name display

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h3>Please Login</h3>
-        </div>
+            <div style={{ margin: "20px" }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder='write email' name="" id="email" />
+                <br />
+                <input type="password" placeholder='write password' name="" id="password" />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+        </div >
     );
 };
 
---
+++ src/components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register Now!</h3>
+            <form>
+                <input type="text" placeholder='your name' name="" id="" />
+                <br />
+                <input type="email" placeholder='write email' name="" id="" />
+                <br />
+                <input type="password" placeholder='write password' name="" id="" />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
